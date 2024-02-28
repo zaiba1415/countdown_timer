0d94b1be1b45a22bee525c16f0c5e58151bd67cf
Zaiba <154451069+zaiba1415@users.noreply.github.com>
2024-02-28 03:53:33 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+async function startTimer() {
+    console.log(`Welcome to the Count down timer`);
+    const timeInput = await inquirer.prompt({
+        type: "input",
+        name: "duration",
+        message: "Enter the duration in Second"
+    });
+    const duration = parseInt(timeInput.duration);
+    let secondsLeft = duration;
+    const timer = setInterval(() => {
+        if (secondsLeft > 0) {
+            console.clear();
+            console.log(`Time remaining ${secondsLeft}`);
+            secondsLeft--;
+        }
+        else {
+            clearInterval(timer);
+            console.clear();
+            console.log(`Timer completed`);
+        }
+    }, 1000);
+}
+startTimer();
